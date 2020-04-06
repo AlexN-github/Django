271248531e70c080331990224f9570a1e94c0e95
Alex <n_alex2001@mail.ru>
2020-04-06 13:51:18 +0300

    Все выполнил

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -5,12 +5,40 @@ def main(request):
     content = { 'title' : {'name' : "Главная"}}
     return render(request, 'index.html', content)
 
-
-def products(request):
-    content = { 'title' : {'name' : "Продукты"}}
-    return render(request, 'products.html', content)
-
-
 def contact(request):
     content = { 'title' : {'name' : "Контакты"}}
     return render(request, 'contact.html', content)
+
+links_menu = [
+    {'href': 'products_all', 'name': 'все'},
+    {'href': 'products_home', 'name': 'дом'},
+    {'href': 'products_office', 'name': 'офис'},
+    {'href': 'products_modern', 'name': 'модерн'},
+    {'href': 'products_classic', 'name': 'классика'},
+]
+content = {'title': {'name': "Продукты"},
+           'links_menu' : links_menu,
+          }
+def products(request):
+    return render(request, 'products.html', content)
+
+def products_all(request):
+    #content = { 'title' : {'name' : "Продукты"}}
+    return render(request, 'products.html', content)
+
+def products_home(request):
+    #content = { 'title' : {'name' : "Продукты"}}
+    return render(request, 'products.html', content)
+
+def products_office(request):
+    #content = { 'title' : {'name' : "Продукты"}}
+    return render(request, 'products.html', content)
+
+def products_modern(request):
+    #content = { 'title' : {'name' : "Продукты"}}
+    return render(request, 'products.html', content)
+
+def products_classic(request):
+    #content = { 'title' : {'name' : "Продукты"}}
+    return render(request, 'products.html', content)
+
