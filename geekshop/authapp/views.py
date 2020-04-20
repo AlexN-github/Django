7397457c03ae7ecb5c2e46f4f31b83f77c7af24b
Alex <n_alex2001@mail.ru>
2020-04-20 14:30:59 +0300

    Выполнено

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -3,10 +3,14 @@ from django.contrib import auth
 from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.urls import reverse
 
+
 def login(request):
     title = 'вход'
 
-    login_form = ShopUserLoginForm(data=request.POST or None) # or None
+    login_form = ShopUserLoginForm(data=request.POST or None)
+
+    next = request.GET['next'] if 'next' in request.GET.keys() else ''
+
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
@@ -14,8 +18,17 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
-            return  HttpResponseRedirect(reverse('main'))
-    content = {'title': title, 'login_form': login_form}
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
+            else:
+                return HttpResponseRedirect(reverse('main'))
+
+    content = {
+        'title': title,
+        'login_form': login_form,
+        'next': next
+    }
+
     return render(request, 'authapp/login.html', content)
 
 def logout(request):
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,11 @@
+import datetime
+import random
+
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from basketapp.models import Basket
+from mainapp.management.commands.fill_db import load_from_json
 from mainapp.models import Product, ProductCategory
 from django.db.models import Sum
 
@@ -10,10 +14,29 @@ def main(request):
     title = 'главная'
     products = Product.objects.all()[:3]
     content = {
-        'title' : title,
+        'title': title,
         'products': products
     }
     return render(request, 'index.html', content)
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+    return same_products
 
 
 def products(request,pk=None):
@@ -23,9 +46,6 @@ def products(request,pk=None):
     basket = []
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
-
-        select_basket_product = basket.aggregate(Sum('quantity'))['quantity__sum']
-        print(select_basket_product)
 
     if pk is not None:
         if pk == 0:
@@ -40,21 +60,46 @@ def products(request,pk=None):
             'category': category,
             'products': products,
             'basket': basket,
-            'select_basket_product': select_basket_product,
         }
         return render(request, 'products_list.html', content)
 
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    #same_products = Product.objects.all()[3:5]
     content = {
         'title': title,
         'links_menu': links_menu,
+        'hot_product': hot_product,
         'same_products': same_products,
         'basket': basket,
-        'select_basket_product': select_basket_product,
     }
 
     return render(request, 'products.html', content)
 
 
+def product(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'product.html', content)
+
+
 def contact(request):
+    title = 'о нас'
+    visit_date = datetime.datetime.now()
+
+    #locations = load_from_json('contact_locations')
+
+    content = {
+        'title': title,
+        'visit_date': visit_date,
+      #  'locations': locations,
+        'basket': get_basket(request.user),
+    }
     return render(request, 'contact.html', content)
